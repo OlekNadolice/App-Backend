1a2319b8913562232233713937579f934ee1a8f3
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-02-02 15:49:46 +0100

    Refactorization

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,43 +1,16 @@
 const express = require("express");
 const User = require("../models/User");
 const passport = require("../passportSetup");
-const { body, validationResult, check } = require("express-validator");
+
 const authRouter = express.Router();
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
+const validation = require("../validation");
 const authController = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/AuthMiddleware");
 const settingsController = require("../controllers/SettingsController");
 const RequestController = require("../controllers/RequestController");
-const { set } = require("mongoose");
-const { urlencoded } = require("express");
+const UsersController = require("../controllers/UsersController");
 
-authRouter.post(
-  "/register",
-  [
-    body("email")
-      .isEmail()
-      .withMessage("Email must be a valid email!")
-      .normalizeEmail()
-      .toLowerCase()
-      .custom(async value => {
-        try {
-          const user = await User.findOne({ email: value });
-          if (user) {
-            return Promise.reject("Email is already used");
-          }
-        } catch (err) {
-          return false;
-        }
-      })
-      .withMessage("email already exists!"),
-    check("password")
-      .isLength({ min: 6 })
-      .withMessage("Password length minimum 6 characters!"),
-    body("name").not().isEmpty().withMessage("Name  field cant be empty!"),
-  ],
-  authController.registerUser
-);
+authRouter.post("/register", validation, authController.registerUser);
 
 authRouter.post("/login", authController.loginHandler);
 
@@ -55,13 +28,7 @@ authRouter.get(
     failureRedirect: "http://localhost:3000/",
     failureMessage: true,
   }),
-  function (req, res) {
-    let { _id: id, name, profileImage } = req.user;
-    const token = jwt.sign({ id: req.user._id }, process.env.JWTPASSWORD);
-    profileImage = btoa(profileImage);
-
-    res.redirect(`http://localhost:3000/veryfied/${token}/${id}/${name}/${profileImage}`);
-  }
+  authController.loginWithGoogle
 );
 
 authRouter.post(
@@ -114,62 +81,17 @@ authRouter.post(
 );
 
 authRouter.get(
+  "/user/:id",
+  authMiddleware.veryfiedToken,
+  authMiddleware.checkToken,
+  UsersController.findSingleUser
+);
+
+authRouter.get(
   "/users",
   authMiddleware.veryfiedToken,
   authMiddleware.checkToken,
-  async (req, res) => {
-    let users = await User.find({});
-    const count = await User.countDocuments();
-
-    users = users.map(element => {
-      return {
-        name: element.name,
-        profileImage: element.profileImage,
-        city: element.city,
-        id: element.id,
-      };
-    });
-
-    if (req.query.name) {
-      const { name } = req.query;
-      const queryString = name.replace(",", " ");
-      users = await User.findOne({ name: queryString });
-    }
-
-    if (req.query.type && req.query.id) {
-      const { id, type } = req.query;
-      users = await User.findOne({ _id: id }).select(type);
-
-      if (Array.isArray(users[type]) && users[type].length > 0) {
-        users = await User.findOne({ _id: id })
-          .select(type)
-          .populate(type, "name profileImage _id");
-        console.log("jest arrayem");
-      }
-    } else if (req.query.id) {
-      const { id } = req.query;
-      users = await User.findOne({ _id: id })
-        .select("name")
-        .select("city")
-        .select("profileImage")
-        .select("friends")
-        .select("friendsRequests");
-    }
-
-    if (req.query.page) {
-      const page = req.query.page || 1;
-      const limit = 8;
-      const skip = (page - 1) * limit;
-      users = await User.find({})
-        .select("name")
-        .select("profileImage")
-        .select("city")
-        .limit(limit)
-        .skip(skip);
-    }
-
-    res.json({ data: users, count: count });
-  }
+  UsersController.getAllUsers
 );
 
 module.exports = authRouter;
